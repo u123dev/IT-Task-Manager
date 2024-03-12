0712b97edd6ea1e9abbad8183f106580f94b37e7
Ant <u123@ua.fm>
2024-03-12 22:06:43 +0200

    create view for home page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Task, Worker, Position
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_tasks = Task.objects.count()
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+        "num_positions": num_positions,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
