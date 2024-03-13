3e55551378d8e5d47d8eec248455ca0a09e3e049
Ant <u123@ua.fm>
2024-03-13 23:24:00 +0200

    add tasks routes to urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from task_manager.views import index, PositionListView, PositionCreateView, PositionUpdateView, PositionDeleteView, \
     TaskTypeListView, TaskTypeCreateView, TaskTypeUpdateView, TaskTypeDeleteView, WorkerListView, WorkerDetailView, \
-    WorkerCreateView, WorkerDeleteView, WorkerUpdateView
+    WorkerCreateView, WorkerDeleteView, WorkerUpdateView, TaskListView, TaskUpdateView, TaskDeleteView, TaskDetailView, \
+    TaskCreateView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -19,6 +20,11 @@ urlpatterns = [
     path("tasktypes/create/", TaskTypeCreateView.as_view(), name="tasktype-create"),
     path("tasktypes/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="tasktype-update"),
     path("tasktypes/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="tasktype-delete"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "task_manager"
