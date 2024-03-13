54d91026df3594429e57ded8a8f76144aece16cf
Ant <u123@ua.fm>
2024-03-13 18:35:43 +0200

    add workers routes to urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from task_manager.views import index, PositionListView, PositionCreateView, PositionUpdateView, PositionDeleteView, \
-    TaskTypeListView, TaskTypeCreateView, TaskTypeUpdateView, TaskTypeDeleteView
+    TaskTypeListView, TaskTypeCreateView, TaskTypeUpdateView, TaskTypeDeleteView, WorkerListView, WorkerDetailView, \
+    WorkerCreateView, WorkerDeleteView, WorkerUpdateView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -9,7 +10,11 @@ urlpatterns = [
     path("positions/create/", PositionCreateView.as_view(), name="position-create"),
     path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
     path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
-    path("", index, name="worker-list"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
     path("tasktypes/", TaskTypeListView.as_view(), name="tasktype-list"),
     path("tasktypes/create/", TaskTypeCreateView.as_view(), name="tasktype-create"),
     path("tasktypes/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="tasktype-update"),
