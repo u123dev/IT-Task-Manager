6d1151ee1582f6e144545363837916640ecb44c9
Ant <u123@ua.fm>
2024-03-13 18:30:05 +0200

    add create, update, search forms for Worker

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from task_manager.models import Worker
 
 
 class PositionSearchForm(forms.Form):
@@ -17,3 +20,35 @@ class TaskTypeSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "search task type"})
     )
+
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "search worker by username"})
+    )
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "position",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+
+class WorkerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "position",
+            "email",
+        )
