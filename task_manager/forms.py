e2f5e61ff2bb8646ecf5d293bd043a126d5c1f4b
Ant <u123@ua.fm>
2024-03-14 11:11:31 +0200

    add validation for deadline date in TaskCreationForm

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -3,6 +3,7 @@ import datetime
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from task_manager.models import Worker, Task
 
@@ -30,7 +31,9 @@ class WorkerSearchForm(forms.Form):
         max_length=255,
         required=False,
         label="",
-        widget=forms.TextInput(attrs={"placeholder": "search worker by username"})
+        widget=forms.TextInput(
+            attrs={"placeholder": "search worker by username"}
+        )
     )
 
 
@@ -70,16 +73,29 @@ class TaskCreationForm(forms.ModelForm):
         initial=expected_deadline_date,
     )
 
-    TRUE_FALSE_CHOICES = ((False, 'No'), (True, 'Yes'))
+    TRUE_FALSE_CHOICES = ((False, "No"), (True, "Yes"))
     is_completed = forms.ChoiceField(
         choices=TRUE_FALSE_CHOICES,
         label="Is Completed ?",
-        initial='',
+        initial="",
         widget=forms.Select(), required=True)
 
     class Meta:
         model = Task
         fields = "__all__"
+
+    def clean_deadline(self):
+        return validate_deadline(self.cleaned_data["deadline"])
+
+
+def validate_deadline(deadline):
+    if deadline < datetime.date.today():
+        raise ValidationError(
+            f"Deadline date should be not less than today: "
+            f"{datetime.date.today().strftime("%d-%m-%Y")}"
+        )
+
+    return deadline
 
 
 class TaskSearchForm(forms.Form):
