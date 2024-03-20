e5524124dd744f78d64fc60188a11f9bcd678daa
Ant <u123@ua.fm>
2024-03-21 00:53:16 +0200

    update settings.py - add url to ALLOWED_HOSTS

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://it-task-manager-iho2.onrender.com",]
 
 INTERNAL_IPS = ["127.0.0.1",]
 
